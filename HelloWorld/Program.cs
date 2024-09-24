a669a997dee6c2bf977dd3c90f5e47f829709175
Tamanna Akther <Akther-T2@ulster.ac.uk>
2024-09-24 14:56:56 +0100

    This is variable types and structures

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int firstScore = 55;
+int secondScore = 4;
+secondScore = firstScore;
+firstScore = -1;
 
-Console.WriteLine("I am Tamanna");
 
-Console.WriteLine("I am stent");
 
-Console.WriteLine("I am student");
+
+Console.WriteLine("The content of firstScore is  "+ firstScore);
+
+Console.WriteLine("The content of secondScore is  " + secondScore);
+
+
+Console.ReadKey();
+
+
+double pi = 3.14159265358979323846; 
+float anotherPi = 3.1415926f;
+
+bool itWorked = true; 
+itWorked = false;
+
+string message = "Hello World!";
+
+message = "Turkey and ham";
+
 
